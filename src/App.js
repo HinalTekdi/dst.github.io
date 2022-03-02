ed58dd7022aa3294cf679b7b7cf77f1aa9681135
Hinal <hinal_d@tekditechnologies.com>
2022-03-02 16:57:16 +0530

    Create & add favicon icon of DST app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='../apple-icon-72x72.png' alt="logo" />
+        <h1>DST-PWA</h1>
       </header>
     </div>
   );
